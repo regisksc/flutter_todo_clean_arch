4f28b3ad6b4de2b523c0b8dcd071a95c35e1139e
Regis Kian <regisksc@gmail.com>
2021-05-28 10:10:59 -0300

    fixed issue

--- presentation/lib/presentation/components/atoms/loader.dart
+++ presentation/lib/presentation/components/atoms/loader.dart
@@ -1,16 +1,19 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../../../presentation.dart';
 
 class Loader extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Platform.isIOS
-      ? CupertinoTheme(
-          data: CupertinoTheme.of(context).copyWith(brightness: Brightness.dark),
-          child: const CupertinoActivityIndicator(),
-        )
-      : const CircularProgressIndicator(backgroundColor: AppColors.white);
+  Widget build(BuildContext context) {
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      return const CircularProgressIndicator(color: AppColors.white);
+    } else {
+      return CupertinoTheme(
+        data: CupertinoTheme.of(context).copyWith(brightness: Brightness.dark),
+        child: const CupertinoActivityIndicator(),
+      );
+    }
+  }
 }
